4f04465d93d4a896dfe9c26703aa60df7d2b4449
Dmitry <dmitry.zaytsev83@gmail.com>
2021-03-31 20:06:27 +0300

    add excursion-brief component

---
+++ src/app/excursion-brief/excursion-brief.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-excursion-brief',
+  templateUrl: './excursion-brief.component.html',
+  styleUrls: ['./excursion-brief.component.css']
+})
+export class ExcursionBriefComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
