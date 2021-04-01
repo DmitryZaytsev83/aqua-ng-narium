3b9d8041ff5145fb245739d7bb9ca6027edd25d2
Dmitry <dmitry.zaytsev83@gmail.com>
2021-04-01 10:50:17 +0300

    add excursion page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {PhotosComponent} from './pages/photos/photos.component';
 import {VideosComponent} from './pages/videos/videos.component';
 import {LandingComponent} from './pages/landing/landing.component';
+import {ExcursionsComponent} from './pages/excursions/excursions.component';
 
 const routes: Routes = [
   {path: '', component: LandingComponent},
   {path: 'фото', component: PhotosComponent},
   {path: 'видео', component: VideosComponent},
-  {path: 'экскурсии', component: LandingComponent},
+  {path: 'экскурсии', component: ExcursionsComponent},
 ];
 
 @NgModule({
