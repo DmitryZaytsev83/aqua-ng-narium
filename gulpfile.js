f2f14b7d91f7b3d745c96d3a9b53265ae93c86e4
Dmitry <dmitry.zaytsev83@gmail.com>
2021-04-05 14:34:37 +0300

    Revert "add gulp to dev dependencies"
    This reverts commit 3eb7256feb9e57a59ce53de30966c50fd8006e5f.

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const {src, dest} = require('gulp');
-
-function defaultTask() {
-  // place code for your default task here
-  return src('src/*.ts')
-    .pipe(dest('/Applications/XAMPP/htdocs/laravel-angular/public/gulp/'));
-}
-
-exports.default = defaultTask
